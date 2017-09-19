a34ff574d227824ef9e35111af7b464fb3df8d95
wentao.li <liwt@certusnet.com.cn>
2017-09-19 04:19:27 -0700

    add login and logout

--- blog/views.py
+++ blog/views.py
@@ -48,12 +48,14 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     print(posts)
     return render(request, 'blog/post_draft_list.html',{'posts':posts})
 
 
+@login_required
 def post_publish(request,pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
