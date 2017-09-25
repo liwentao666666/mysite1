f05509e998075466ccb752c719641a4aebff221a
wentao.li <liwt@certusnet.com.cn>
2017-09-25 04:05:05 -0700

    update 最新文章

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
@@ -32,6 +33,9 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
