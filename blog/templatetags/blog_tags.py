be872914451a241c72443fef966756bdabd80897
wentao.li <liwt@certusnet.com.cn>
2017-09-26 00:34:19 -0700

    add number of post in category

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
-from ..models import Post,Category
+from ..models import Post,Category,Tag
 from django import template
+from django.db.models.aggregates import Count
 
 register = template.Library()
 
@@ -14,4 +15,10 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    #return Category.objects.annotate(num_posts=Count('post')).filter(num_post__gt==0)
+    return Category.objects.annotate(num_posts=Count('post'))
+
+@register.simple_tag
+def get_tags():
+    #return Category.objects.annotate(num_posts=Count('post')).filter(num_post__gt==0)
+    return Tag.objects.annotate(num_posts=Count('post'))
