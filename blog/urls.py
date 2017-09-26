5edc056861bbe6571647f29abf06579bcc586fd8
wentao.li <liwt@certusnet.com.cn>
2017-09-25 23:04:37 -0700

    add class based views

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ import logging
 
 #app_name='blog'
 urlpatterns = [
-    url(r'^$',views.post_list, name='post_list'),
+    #url(r'^$',views.post_list, name='post_list'),
+    url(r'^$',views.IndexView.as_view(), name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail,name='post_detail'),
     url(r'^post/new/$',views.post_new,name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/.*', views.post_edit, name='post_edit'),
@@ -14,6 +15,8 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
     url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
     url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
-    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
-    url(r'^category/(?P<pk>[0-9]+)/$',views.category,name='category'),
+    #url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
+    #url(r'^category/(?P<pk>[0-9]+)/$',views.category,name='category'),
+    url(r'^category/(?P<pk>[0-9]+)/$',views.CategoryView.as_view(),name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,72 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Post,Comment,Category
 from .forms import PostForm,CommentForm
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
-def post_list(request):
-    #posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    posts=Post.objects.all().order_by('published_date')
-    print(posts)
-    return render(request,'blog/post_list.html',{'posts':posts})
+
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name= 'posts'
+
+    def get_queryset(self):
+        return super(IndexView,self).get_queryset().all().order_by('published_date')
+
+class CategoryView(ListView):
+    model= Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk = self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate).order_by('-created_date')
+
+class ArchivesView(ListView):
+    model= Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return super(ArchivesView, self).get_queryset().filter(created_date__year=self.kwargs.get('year'),created_date__month=self.kwargs.get('month')).order_by('created_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self,request, *args, **kwargs):
+        response= super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self,queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                    extensions=[
+                                        'markdown.extensions.extra',
+                                        'markdown.extensions.codehilite',
+                                        'markdown.extensions.toc',
+                                        ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView,self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form':form,
+            'comment_list':comment_list
+        })
+        return context
+
+#def post_list(request):
+#    #posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#    posts=Post.objects.all().order_by('published_date')
+#    print(posts)
+#    return render(request,'blog/post_list.html',{'posts':posts})
 
 def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
@@ -101,13 +160,13 @@ def comment_remove(request, pk):
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
 
-def archives(request, year ,month):
-    posts = Post.objects.filter(created_date__year=year,
-                                    created_date__month=month
-                                    ).order_by('created_date')
-    return render(request,'blog/post_list.html',{'posts':posts})
-
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    posts= Post.objects.filter(category=cate).order_by('-created_date')
-    return render(request, 'blog/post_list.html', context={'posts':posts})
+#def archives(request, year ,month):
+#    posts = Post.objects.filter(created_date__year=year,
+#                                    created_date__month=month
+#                                    ).order_by('created_date')
+#    return render(request,'blog/post_list.html',{'posts':posts})
+#
+#def category(request, pk):
+#    cate = get_object_or_404(Category, pk=pk)
+#    posts= Post.objects.filter(category=cate).order_by('-created_date')
+#    return render(request, 'blog/post_list.html', context={'posts':posts})
